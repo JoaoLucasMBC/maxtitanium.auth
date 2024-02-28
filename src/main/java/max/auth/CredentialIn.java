9012d8b4c0a4c49201047821001b72e6eb67da81
JoaoLucasMBC <joaolucasmbcadorniga@gmail.com>
2024-02-28 11:47:17 -0300

    FEATURE: adding essential structure for auth

---
+++ src/main/java/max/auth/CredentialIn.java
@@ -0,0 +1,10 @@
+package max.auth;
+
+import lombok.Builder;
+import lombok.experimental.Accessors;
+
+@Builder @Accessors(fluent = true, chain = true)
+public record CredentialIn (
+    String email,
+    String password
+) { }
